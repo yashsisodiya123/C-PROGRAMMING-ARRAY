9203106c330413f21a372c3d709c4d888696f378
yashsisodiya123 <sisodiyay35@gmail.com>
2025-09-12 01:40:30 +0530

    Add files via upload

---
+++ accept 10 values and print 4th,7th and 9th value.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+int a[10],i;
+
+printf("enter the elements of array= ");
+
+for(i=0;i<10;i++)
+{scanf("%d,", &a[i]);
+}
+
+printf("4th value= %d\n", a[3]);
+printf("7th value= %d\n", a[6]);
+printf("9th value= %d\n", a[8]);
+}
---
+++ accept 5 values and print them.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+int a[5],i;
+
+printf("enter the elements of array= ");
+
+for(i=0;i<5;i++)
+{scanf("%d", &a[i]);
+}
+
+printf("elements of array are= ");
+for(i=0;i<5;i++)
+{printf("%d\n", a[i]);
+}
+}
